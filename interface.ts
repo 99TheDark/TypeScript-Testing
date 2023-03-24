060ffa86b13b0c70642085a4ce57460cc7c59966
TheDark <104472362+99TheDark@users.noreply.github.com>
2023-03-23 23:38:45 -0700

    Create interface.ts

---
+++ interface.ts
@@ -0,0 +1,15 @@
+interface Person {
+    name: string,
+    age: number,
+    tired: boolean
+}
+
+function isTired(person: Person) : boolean {
+    return person.tired;
+}
+
+console.log(isTired({
+    name: "TheDark",
+    age: 14,
+    tired: true
+}));
